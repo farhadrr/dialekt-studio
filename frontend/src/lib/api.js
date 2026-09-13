120b1d98873a1431ab6f6d6756b36439c5b63e00
Farhad <farhad10180@gmail.com>
2026-09-14 02:56:25 +0300

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,9 @@
+
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-export const API = `${BACKEND_URL}/api`;
+// تم وضع رابط افتراضي مباشر لكي يعمل السيرفر ولا يظهر فارغاً
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "";
+export const API = BACKEND_URL ? `${BACKEND_URL}/api` : "/api";
 
 export const api = axios.create({ baseURL: API });
 
